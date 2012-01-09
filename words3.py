c9f260724ce73dbafef215cdf3f582f7fc480677
Jessica McKellar <jesstess@mit.edu>
2012-01-08 19:18:56 -0500

    words3: move vowels check into a function.
    BPW5 attendees said this version was easier to understand.

--- words3.py
+++ words3.py
@@ -2,12 +2,13 @@ import scrabble
 
 VOWELS = ['a', 'e', 'i', 'o', 'u']
 
-# Print all words that have all 5 vowels.
-for word in scrabble.wordlist:
-    has_all_vowels = True
+def has_all_vowels(word):
     for vowel in VOWELS:
         if vowel not in word:
-            has_all_vowels = False
-            break
-    if has_all_vowels:
+            return False
+    return True
+
+# Print all words that have all 5 vowels.
+for word in scrabble.wordlist:
+    if has_all_vowels(word):
         print word
