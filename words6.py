f2384fd444867625a25f65d7460cc7c0d3f6b615
Jessica McKellar <jesstess@mit.edu>
2013-08-24 09:01:08 -0400

    words6: fix comment typo.

--- words6.py
+++ words6.py
@@ -1,8 +1,8 @@
 import re
 import scrabble
 
-# Use a regular expression to find an print all words matching certain character
-# constraints.
+# Use a regular expression to find and print all words matching certain
+# character constraints.
 pattern = re.compile("^e.g....r$")
 for word in scrabble.wordlist:
     if pattern.search(word):
